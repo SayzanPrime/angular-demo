3465f200a2afd60f75d53a0f67cfac43d7b72a83
Abdessalam Rhatous <a.rhatous@isicod.net>
2023-01-12 20:10:33 +0100

    we

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-demo';
+
+  readonly apiUrl = environment.ApiUrl;
+
+  users: any = [];
+
+  constructor(
+    private fb: FormBuilder,
+    private http: HttpClient
+  ) { }
+
+  userForm = this.fb.group({
+    firstName: [''],
+    lastName: [''],
+  });
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  save() {
+    this.http.post(this.apiUrl + 'save', this.userForm.value).subscribe(savedUser => {
+      this.getUsers();
+    });
+  }
+
+  getUsers() {
+    this.http.get(this.apiUrl + 'all').subscribe(users => {
+      this.users = users;
+    });
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  ApiUrl: 'http://localhost:8080/users/',
   production: false
 };
 
